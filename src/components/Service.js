ce622a6af3016cf0c70e012d756659c359af6f20
unknown <oyeleyeoyedayo@gmail.com>
2021-09-30 11:16:23 +0100

    remove animation from service

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react';
 import defaultImg from '../images/defaultBcg.jpeg';
-import Aos from 'aos';
+//import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 
 export default function Service( { service } ) {
     const { name, images, codeUrl, serviceUrl } = service;
-        useEffect(()=>{
+    /* useEffect(()=>{
             Aos.init({duration:2000});
-        },[]);
+        },[]); 
+    */
      
     return (
         
         <article className='project'>
-            <div data-aos='fade-up' className='img-container'>
+            <div /* data-aos='fade-up'*/ className='img-container'>
                 <img src={images[0] || defaultImg} alt='single project'/>
                 <a href={`${codeUrl}`} className='btn-primary  project-link'>
                     View
