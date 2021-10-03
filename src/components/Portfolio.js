0cac882f0b9de9863d825f0d365e2a54010abf99
unknown <oyeleyeoyedayo@gmail.com>
2021-10-03 18:28:17 +0100

    Update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,7 +25,7 @@ export default function Portfolio () {
                         <article className='portfolio' >
                             <span><FaEuroSign/></span>
                             <h6>Financial Education</h6>
-                            <p>We recently acquired our first property (a new build 3 bedroom in Ireland) in our 20s. To help you work towards your journey of financial independence, we can organize a 121 session where you learn the following:</p>
+                            <p>We recently made a <a href='https://youtu.be/MjGOpq7E4Bs'>video</a> about acquiring our first property (a new build 3 bedroom in Ireland) in our 20s. To help you work towards your journey of financial independence, we can organize a 121 session where you learn the following:</p>
                             <p>Saving towards buying a house</p>
                             <p>Explaining mortgage application process in details through our experience.</p>
                         </article>
@@ -40,7 +40,7 @@ export default function Portfolio () {
                             <span><FaLaptop/></span>
                             <h6>WEB DEVELOPMENT</h6>
                             <p>We can help you build your responsive and maintainable web application with the latest technologies in the tech industry.</p>
-                            <p>With bachelor’s and 3 master’s degrees in Education and Technical disciplines from 3 different countries, as well as working for one of the biggest tech companies ever, we have got the right educational background, experiences and skills to educate and serve you.</p>
+                            <p>With bachelor’s, 3 master’s degrees and certifications in Education and Technical disciplines from 3 different countries, as well as working for one of the biggest tech companies ever, we have got the right educational background, experiences and skills to educate and serve you.</p>
                         </article> 
                 </div>
             </section>
